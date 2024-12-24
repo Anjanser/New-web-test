15dfb5eaf87ae26051f8ed3a55ae52f496b54a87
ʙᴀᴅʙᴏʏ-ᴀɴᴊ <kalachad131@gmail.com>
2024-12-24 10:29:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Scripting for extra effects (optional)
+document.addEventListener("DOMContentLoaded", function() {
+    const welcomeMessage = document.querySelector('.welcome-message');
+    welcomeMessage.textContent = "Welcome to Anjan Official Page";
+});
